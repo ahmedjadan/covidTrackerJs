70e988fc8158bcd9e4bc4d2fe774908eaba3ad6b
ahmedjadan <a.jadan7@gmail.com>
2020-04-26 18:16:17 +0200

    adding favicon

--- app.js
+++ app.js
@@ -2,18 +2,7 @@ const API_1 = 'https://corona.lmao.ninja/v2/all/';
 const API_2 = 'https://corona.lmao.ninja/v2/countries/';
 
 
-// fetch(API_1).then((response) => {
-//     return response.json();
-//     })
-//     .then( data => {
-//         console.log(data)
-//     })
 
-//     .catch((err) => {
-//         console.log('rejected', err)
-//     })
-
-//Async 
 
 const getCovidData = async () => {
 
@@ -98,45 +87,45 @@ function filterSearch() {
   }
 
 
-   sortTable = (n) => {
-    var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
-    table = document.getElementById("table_1");
-    switching = true;
-    dir = "asc";
+  //  sortTable = (n) => {
+  //   var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+  //   table = document.getElementById("table_1");
+  //   switching = true;
+  //   dir = "asc";
    
-    while (switching) {
-      switching = false;
-      rows = table.rows;
+  //   while (switching) {
+  //     switching = false;
+  //     rows = table.rows;
      
-      for (i = 1; i < (rows.length - 1); i++) {
-        shouldSwitch = false;
+  //     for (i = 1; i < (rows.length - 1); i++) {
+  //       shouldSwitch = false;
        
-        x = rows[i].getElementsByTagName("td")[n];
-        y = rows[i + 1].getElementsByTagName("td")[n];
+  //       x = rows[i].getElementsByTagName("td")[n];
+  //       y = rows[i + 1].getElementsByTagName("td")[n];
       
-        if (dir == "asc") {
-          if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-            shouldSwitch = true;
-            break;
-          }
-        } else if (dir == "desc") {
-          if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
-            shouldSwitch = true;
-            break;
-          }
-        }
-      }
-      if (shouldSwitch) {
+  //       if (dir == "asc") {
+  //         if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+  //           shouldSwitch = true;
+  //           break;
+  //         }
+  //       } else if (dir == "desc") {
+  //         if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+  //           shouldSwitch = true;
+  //           break;
+  //         }
+  //       }
+  //     }
+  //     if (shouldSwitch) {
        
-        rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-        switching = true;
-        switchcount ++;
-      } else {
+  //       rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+  //       switching = true;
+  //       switchcount ++;
+  //     } else {
         
-        if (switchcount == 0 && dir == "asc") {
-          dir = "desc";
-          switching = true;
-        }
-      }
-    }
-  }
+  //       if (switchcount == 0 && dir == "asc") {
+  //         dir = "desc";
+  //         switching = true;
+  //       }
+  //     }
+  //   }
+  // }
